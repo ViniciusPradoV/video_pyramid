5fa34f08c2fa042344ebdc14d5fcccfc8277e4bd
ViniciusPradoV <31973401+ViniciusPradoV@users.noreply.github.com>
2019-10-23 22:16:48 -0300

    home view + insert_video

--- video_pyramid/views/views.py
+++ video_pyramid/views/views.py
@@ -1,10 +1,41 @@
 from pyramid.view import view_config, view_defaults
 from pymongo import MongoClient
 
-@view_defaults(renderer='templates/layout.jinja2')
+class Database(object):
+
+    def __init__(self):
+        self.client = MongoClient()
+        self.db = self.client["videos"]
+        self.collection = self.db["videos"]
+
+    def insert_video(self, title, theme):
+        video =
+        {
+        "title": title,
+        "theme": theme,
+        "like": 0,
+        "dislike": 0,
+        "total_score": 0,
+
+        }
+
+        self.collection.insert(video)
+
+        return("video saved")
+
 class Views:
     def __init__(self, request):
         self.request = request
 
+    @view_config(route_name='home', renderer='templates/home.jinja2')
     def home(self):
-        return{'name': 'Home View'}
+        db = Database()
+        if self.request.POST.get('title'):
+            title = str(self.request.POST.get('title'))
+            theme = str(self.request.POST.get('theme'))
+            db.insert_video(title, theme)
+
+            videos = []
+            for video in self.collection.find():
+            videos.append(video)
+        return videos
